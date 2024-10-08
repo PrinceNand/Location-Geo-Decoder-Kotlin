535131514868bf12564268cd80eb829c6685c734
Ashley <ashnand2000@gmail.com>
2024-10-08 15:14:26 +0530

    Location: 2.LocationUtils to check the permission request

---
+++ app/src/main/java/com/project/locationdecoder/LocationUtils.kt
@@ -0,0 +1,15 @@
+package com.project.locationdecoder
+
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+
+class LocationUtils(val context: Context) {
+
+    // To check the permission request
+    fun hasLocationPermission(context: Context): Boolean{
+        return ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                &&
+                ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+}
