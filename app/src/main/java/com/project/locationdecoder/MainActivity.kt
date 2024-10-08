5c568dcebacfbb62c7721e180788ec4e8f1608d6
Ashley <ashnand2000@gmail.com>
2024-10-08 15:45:14 +0530

    Location: 6.Call The location from the main activity by sending all required parameters

--- app/src/main/java/com/project/locationdecoder/MainActivity.kt
+++ app/src/main/java/com/project/locationdecoder/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat
 import com.project.locationdecoder.ui.theme.LocationDecoderTheme
@@ -35,11 +36,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    MyApp()
                 }
             }
         }
     }
+}
+
+@Composable
+fun MyApp() {
+    val context = LocalContext.current
+    val location = LocationUtils(context)
+    LocationDisplay(locationUtils = location, context = context)
 }
 
 @Composable
