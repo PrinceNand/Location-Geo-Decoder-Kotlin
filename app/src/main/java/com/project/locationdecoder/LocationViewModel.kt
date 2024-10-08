caf180b0bd6ca4bd4b272a28f90fd0adc78fc8c9
Ashley <ashnand2000@gmail.com>
2024-10-08 15:55:39 +0530

    Location: 8.Creating LocationViewModel to store and use data

---
+++ app/src/main/java/com/project/locationdecoder/LocationViewModel.kt
@@ -0,0 +1,16 @@
+package com.project.locationdecoder
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class LocationViewModel: ViewModel() {
+    private val _locationData = mutableStateOf<LocationData?>(null)
+
+    val locationData : State<LocationData?> = _locationData
+
+    fun updateLocation(newLocationData: LocationData){
+        _locationData.value = newLocationData
+    }
+
+}
