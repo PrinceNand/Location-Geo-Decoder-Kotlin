3ffab634b304b4e6fe54a16be079e2cf5432295d
Ashley <ashnand2000@gmail.com>
2024-10-08 15:31:15 +0530

    Location: 3.UI to fetch location

--- app/src/main/java/com/project/locationdecoder/MainActivity.kt
+++ app/src/main/java/com/project/locationdecoder/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.project.locationdecoder
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.project.locationdecoder.ui.theme.LocationDecoderTheme
@@ -19,11 +26,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LocationDecoderTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
                 }
             }
         }
@@ -31,17 +38,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LocationDisplay(locationUtils: LocationUtils, context: Context) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LocationDecoderTheme {
-        Greeting("Android")
+        Text(text = "Location Not Available")
+
+        Button(onClick = {
+            if (locationUtils.hasLocationPermission(context)) {
+                // Permission granted
+
+            } else {
+                // Request for permission
+
+            }
+        }) {
+            Text(text = "Get Location")
+        }
     }
 }
